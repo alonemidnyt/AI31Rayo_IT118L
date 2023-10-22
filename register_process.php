bb4c6b789c8316446ee3ee26c54aa544a55519f7
Ryan Christian Rayo <130733448+alonemidnyt@users.noreply.github.com>
2023-10-22 17:41:01 +0800

    Initial Commit

---
+++ register_process.php
@@ -0,0 +1,29 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'integ';
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $username, $email, $password);
+
+if ($stmt->execute()) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
